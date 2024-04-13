9fa0e3f167701eeaf1253d12e6fb8f933c2984a4
Francisco Toloza <ftoloza.26@gmail.com>
2024-04-12 20:44:01 -0400

    Este es mi primer commit

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-mensaje = "Hola Mundo"
+# mensaje = "Hola Mundo"
 
-print(mensaje)
+# ACTIVIDAD 1
+# print(mensaje)
+# print("BIENVENIDO AL MUNDO DE LA PROGRAMACION") 
+# nombre = input("Ingresa tu nombre\n")
+# print(f"Bienvenido",nombre)
+
+# ACTIVIDAD 2
+# x = int(input("Dame un numero"))
+# formula = (x**2+3*x+1)/4
+# print(f"El resultado de la ecuacion es {formula} ")
+
+# ACTIVIDAD 3
+# nombre = input("Ingresa tu nombre\n")
+# RUT = input("Ingresa tu rut\n")
+# correo = input("Ingresa tu  correo\n")
+# telefono = input("Ingresa tu telefono\n")
+# print(F"NOMBRE:               {nombre}")
+# print(f"RUT:                  {RUT}")
+# print(f"CORREO:               {correo}")
+# print(f"TELEFONO:             {telefono}")
